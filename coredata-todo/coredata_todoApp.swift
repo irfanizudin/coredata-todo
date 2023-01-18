d566e68aafabcb274809ee1e7e787d1df9e94330
irfanizudin <irfanizzudyn@gmail.com>
2023-01-18 09:59:07 +0700

    Initial Commit

---
+++ coredata-todo/coredata_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  coredata_todoApp.swift
+//  coredata-todo
+//
+//  Created by Irfan Izudin on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct coredata_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
